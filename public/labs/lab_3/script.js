b6462d5d356891951998a006ba7e355222f93127
shrutik1999 <kshruti1999@gmail.com>
2021-02-13 17:48:50 -0500

    cleaning up javascript; copying my code from jsfiddle

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,18 +1,35 @@
-const imgul = document.querySelector('.collection')
 const allimages = document.querySelectorAll('img');
 
 const carouselArray = [];
+// eslint-disable-next-line no-plusplus
 for (let i = 0; i < allimages.length; i++) {
   carouselArray.push(allimages[i].src);
 }
 
-console.log(carouselArray); //doesnt work
-
 let currentslide = 0;
 
-document.querySelector('.left').onclick = () => {
-  if (currentslide = 0) {
-    collection.style.marginRight = '260px';
-    currentslide += 3;
+document.querySelector('.right').onclick = () => {
+  currentslide += 3;
+  if (currentslide > 6) {
+    currentslide = 6;
+  } else if (currentslide == 6) {
+    document.getElementById('image1').src = carouselArray[currentslide - 2];
+    document.getElementById('image2').src = carouselArray[currentslide - 1];
+    document.getElementById('image3').src = carouselArray[currentslide];
+  } else {
+    document.getElementById('image1').src = carouselArray[currentslide];
+    document.getElementById('image2').src = carouselArray[currentslide + 1];
+    document.getElementById('image3').src = carouselArray[currentslide + 2];
   }
-}
+};
+
+document.querySelector('.left').onclick = () => {
+  currentslide -= 3;
+  if (currentslide < 0) {
+    currentslide = 0;
+  } else {
+    document.getElementById('image1').src = carouselArray[currentslide];
+    document.getElementById('image2').src = carouselArray[currentslide + 1];
+    document.getElementById('image3').src = carouselArray[currentslide + 2];
+  }
+};
