e954ca02a1e7496bdf6a13007648eeb0dc0b9eaa
shrutik1999 <kshruti1999@gmail.com>
2021-02-12 22:43:19 -0500

    updating all files from JS fiddle

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,7 +1,28 @@
 /* Put your javascript in here */
 
-const slideArray = [];
-for (let i = -0; i < document.querySelectorAll('.images')).length; i++){
-    slideArray.push(document.querySelectorAll('.images')[i].src); //fix src part
+const imgul = document.querySelector('.collection')
+const allimages = document.querySelectorAll('.images');
+
+const carouselArray = [];
+for (let i = 0; i < allimages.length; i++) {
+  carouselArray.push(allimages[i].src);
 }
 
+console.log(carouselArray); //doesnt work
+
+let currentslideindex = -1;
+
+allimages.onclick = function () {
+	if (currentslideindex<3) {
+  	
+  }
+
+}
+
+/*allimages.onclick = function () {
+	if (currentslideindex<=carouselArray.length) {
+  	currentslideindex ++;
+  
+  
+	}
+}*/
