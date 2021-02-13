4e9f93cb54aa6bf6cdf1e1628195e83fa000c0b6
shrutik1999 <kshruti1999@gmail.com>
2021-02-12 20:59:50 -0500

    linked css properly

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -2,5 +2,6 @@
 
 const slideArray = [];
 for (let i = -0; i < document.querySelectorAll('.images')).length; i++){
-    slideArray.push(document.querySelectorAll('.images')[i].dataset.background);
+    slideArray.push(document.querySelectorAll('.images')[i].src); //fix src part
 }
+
