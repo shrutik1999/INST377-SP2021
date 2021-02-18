5756b2dab36e73cc2a2c0b43b276b0f7403b898d
shrutik1999 <kshruti1999@gmail.com>
2021-02-18 17:58:36 -0500

    following the video, making adjustments

--- public/flappy/script.js
+++ public/flappy/script.js
@@ -4,8 +4,10 @@ document.addEventListener('DOMContentLoaded' , () => {
   const ground = document.querySelector('.ground');
 
   let birdLeft = 220;
-  let birdBottom = 100;
+  let birdBottom = 200; /* adjusted so bird starts higher on screen */
   let gravity = 2;
+  let isGameOver = false;
+  let gap = 430;
 
   function startGame() {
     birdBottom -= gravity;
@@ -13,7 +15,7 @@ document.addEventListener('DOMContentLoaded' , () => {
     bird.style.left = birdLeft + 'px';
   }
 
-  let timerID = setInterval(startGame, 20);
+  let gameTimerID = setInterval(startGame, 20);
 
   function control (e) {
     if (e.keyCode === 32) {
@@ -36,10 +38,45 @@ document.addEventListener('DOMContentLoaded' , () => {
     let randomHeight = Math.random() * (230 - 80) + 80; /* had to be adjusted */
     let obstacleBottom = randomHeight;
     const obstacle = document.createElement('div');
-    obstacle.classList.add('obstacle');
+    const topObstacle = document.createElement('div');
+    if (!isGameOver) {
+      obstacle.classList.add('obstacle');
+      topObstacle.classList.add('topObstacle');
+    }
     gameDisplay.appendChild(obstacle);
+    gameDisplay.appendChild(topObstacle);
     obstacle.style.left = obstacleLeft + 'px';
+    topObstacle.style.left = obstacleLeft + 'px';
     obstacle.style.bottom = obstacleBottom + 'px';
+    topObstacle.style.bottom = obstacleBottom + gap + 'px';
+
+    function moveObstacle() {
+      obstacleLeft -= 2;
+      obstacle.style.left = obstacleLeft + 'px';
+      topObstacle.style.left = obstacleLeft + 'px';
+
+      if (obstacleLeft === -60) {
+        clearInterval(timerId);
+        gameDisplay.removeChild(obstacle);
+        gameDisplay.removeChild(topObstacle);
+      }
+      // eslint-disable-next-line max-len
+      if ((obstacleLeft > 200) && (obstacleLeft < 280) && (birdLeft === 220)
+      && ((birdBottom < obstacleBottom + 150) || (birdBottom > obstacleBottom + gap - 215))
+      || (birdBottom === 80)) { /* had to adjusted from 0 from example */
+        gameOver();
+        clearInterval(timerId); /* not working the bar still moves */
+      }
+    }
+    let timerID = setInterval(moveObstacle, 20);
+    if (!isGameOver) setTimeout(generateObstacle, 3000);
   }
   generateObstacle();
+
+  function gameOver() {
+    clearInterval(gameTimerID);
+    console.log('game over');
+    isGameOver = true;
+    document.removeEventListener('keyup', control);
+  }
 });
