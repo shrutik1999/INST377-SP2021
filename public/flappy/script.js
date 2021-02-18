11bfa0fe73b091bf7c52bd5cfa34a66fde196fb4
shrutik1999 <kshruti1999@gmail.com>
2021-02-18 02:49:28 -0500

    created obstacle w random heights, does not move left/right yet

---
+++ public/flappy/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded' , () => {
+  const bird = document.querySelector('.bird');
+  const gameDisplay = document.querySelector('.game-container');
+  const ground = document.querySelector('.ground');
+
+  let birdLeft = 220;
+  let birdBottom = 100;
+  let gravity = 2;
+
+  function startGame() {
+    birdBottom -= gravity;
+    bird.style.bottom = birdBottom + 'px';
+    bird.style.left = birdLeft + 'px';
+  }
+
+  let timerID = setInterval(startGame, 20);
+
+  function control (e) {
+    if (e.keyCode === 32) {
+      jump();
+    }
+  }
+
+  function jump() {
+    if (birdBottom < 500) {
+      birdBottom += 50;
+      bird.style.bottom = birdBottom + 'px';
+      console.log(birdBottom);
+    }
+  }
+
+  document.addEventListener('keyup', control);
+
+  function generateObstacle() {
+    let obstacleLeft = 500;
+    let randomHeight = Math.random() * (230 - 80) + 80; /* had to be adjusted */
+    let obstacleBottom = randomHeight;
+    const obstacle = document.createElement('div');
+    obstacle.classList.add('obstacle');
+    gameDisplay.appendChild(obstacle);
+    obstacle.style.left = obstacleLeft + 'px';
+    obstacle.style.bottom = obstacleBottom + 'px';
+  }
+  generateObstacle();
+});
