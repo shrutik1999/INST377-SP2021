224060a044bff70e2dd19695b75a5f66142a2f49
shrutik1999 <kshruti1999@gmail.com>
2021-03-10 15:57:48 -0500

    pasted in mapint info

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,6 +1,15 @@
 function mapInit() {
+  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
   // follow the Leaflet Getting Started tutorial here
-  return map;
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic2hydXRpa3VtYXI5OSIsImEiOiJja20zd2J6OXowYXpsMm9vYTlxaDgwbWt2In0.k9O3LYXlgB5gC855Nz8yTA' /* check abt this */
+  }).addTo(mymap);
+  return mymap;
 }
 
 async function dataHandler(mapObjectFromFunction) {
