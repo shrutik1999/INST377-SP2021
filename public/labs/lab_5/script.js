43d5de5026d78a7763f301e09eba5e4869a2b9cb
shrutik1999 <kshruti1999@gmail.com>
2021-03-12 20:37:46 -0500

    copied and pasted current code from jsfiddle

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,5 +1,5 @@
 function mapInit() {
-  const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  const mymap = L.map('mapid').setView([38.9897, -76.9378], 13);
   // follow the Leaflet Getting Started tutorial here
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,23 +14,30 @@ function mapInit() {
 
 async function dataHandler(mapObjectFromFunction) { /* pasted in assignment 1 code */
   const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+  
+  const request = await fetch(endpoint)
+  	const restaurants = await request.json();
 
-  const restaurants = [];
-
-  fetch(endpoint)
-    .then((blob) => blob.json())
-    .then((data) => restaurants.push(...data))
-
-  function findMatches(wordToMatch, restaurants) {
+  /*function findMatches(wordToMatch, restaurants) {
     return restaurants.filter((place) => {
       const regex = new RegExp(wordToMatch, 'gi');
-      return place.name.match(regex) || place.category.match(regex)
+      return place.zip.match(regex)
     });
-  }
+  }*/ /*repetitive and unnecessary */
+
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+  const buttonEvent = document.getElementById('submit');
 
   function displayMatches() {
-    const matchArray = findMatches(this.value, restaurants);
-    const html = matchArray.map((place) => {
+  	const filtered = restaurants.filter((record) => record.zip.includes(searchInput.value) && record.geocoded_column_1);
+    const sliced = filtered.slice(0, 5);
+    
+    sliced.forEach((item) => {
+    	const longLat = item.geocoded_column_1.coordinates;
+      const marker = L.marker([longLat[1], longLat[0]]).addTo(mapObjectFromFunction);
+      
+    	const html = sliced.map((place) => {
       const regex = new RegExp(this.value, 'gi');
       const nameName = place.name.replace(regex, `<span class="h1">${this.value}</span>`);
       const categoryName = place.category.replace(regex, `<span class="h1">${this.value}</span>`);
@@ -39,18 +46,15 @@ async function dataHandler(mapObjectFromFunction) { /* pasted in assignment 1 co
             <span class="name">${nameName}</span><br>
             <span class="category">${categoryName}</span><br>
             <span class="address">${place.address_line_1} </span>
+            <span class="zip">${place.zip}</span>
           </li>
           `;
     }).join('');
     // eslint-disable-next-line no-use-before-define
-    suggestions.innerHTML = html;
+    suggestions.innerHTML = html;  
+    });
   }
-
-  const searchInput = document.querySelector('.search');
-  const suggestions = document.querySelector('.suggestions');
-
-  searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', displayMatches);
+    buttonEvent.addEventListener('submit', displayMatches);
 }
 
 async function windowActions() {
